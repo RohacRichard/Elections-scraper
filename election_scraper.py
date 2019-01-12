1832b8d494c82fd9fc9d49893854c7d30a95625a
Richard Roháč <44978845+RohacRichard@users.noreply.github.com>
2019-01-12 08:38:13 +0100

    Add files via upload

--- election_scraper.py
+++ election_scraper.py
@@ -12,13 +12,13 @@ def greeting():
 	print("="*46)
 
 
-def generate_links(td_header, soup):
+def generate_links(td_headers, soup):
 	"""Generates links with the results of municipalities to iterate over.
 	Takes the headers and generated soup as arguments."""
 
 	generated_links = []
 
-	for td in soup.find_all("td", {"class":"cislo", "headers":td_header}):
+	for td in soup.find_all("td", {"class":"cislo", "headers":td_headers}):
 		new_link = "https://volby.cz/pls/ps2017nss/{}".format(td.find("a")["href"])
 
 		# A check whether there is a same URL already in list.
@@ -28,13 +28,13 @@ def generate_links(td_header, soup):
 	return generated_links
 
 
-def get_code(td_header, soup):
+def get_code(td_headers, soup):
 	"""Gets the code of municipality.
 	Takes the headers and generated soup as arguments."""
 
 	generated_codes = []
 
-	for td in soup.find_all("td", {"class":"cislo", "headers":td_header}):
+	for td in soup.find_all("td", {"class":"cislo", "headers":td_headers}):
 		muni_code = (td.find("a").contents)[0]
 
 		# A check whether there is a same code already in list.
@@ -47,10 +47,10 @@ def get_code(td_header, soup):
 def get_name(soup):
 	"""Gets the municipality name, takes soup as argument."""
 
-	td_header = ["t1sa1 t1sb2", "t2sa1 t2sb2", "t3sa1 t3sb2"]
+	td_headers = ["t1sa1 t1sb2", "t2sa1 t2sb2", "t3sa1 t3sb2"]
 
 	generated_names = [td.contents[0] for td 
-					   in soup.find_all("td", {"headers":td_header})]
+					   in soup.find_all("td", {"headers":td_headers})]
 
 	return generated_names
 
@@ -62,10 +62,10 @@ def base_link_ops(base_link):
 	response = requests.get(base_link)
 	soup = BS(response.text, "html.parser")
 
-	td_header = ["t1sa1 t1sb1", "t2sa1 t2sb1", "t3sa1 t3sb1"]
+	td_headers = ["t1sa1 t1sb1", "t2sa1 t2sb1", "t3sa1 t3sb1"]
 
-	links = generate_links(td_header, soup)
-	codes = get_code(td_header, soup)
+	links = generate_links(td_headers, soup)
+	codes = get_code(td_headers, soup)
 	names = get_name(soup)
 
 	return links, codes, names
@@ -76,10 +76,10 @@ def create_header(soup):
 
 	# Defined header, to which we will append polit. parties.
 	header = ["Code", "Location", "Registered", "Envelopes", "Valid"]
-	td_header = ["t1sa1 t1sb2", "t2sa1 t2sb2"]
+	td_headers = ["t1sa1 t1sb2", "t2sa1 t2sb2"]
 
 	# The empty class attribute fixes problem with empty table row.
-	party_name = soup.find_all("td", {"class":"", "headers":td_header})
+	party_name = soup.find_all("td", {"class":"", "headers":td_headers})
 
 	party_name_list = [name.contents[0] for name in party_name]	
 
@@ -108,18 +108,18 @@ def get_voter_info(soup):
 def get_votes(soup):
 	"""Gets info about votes for polit. parties, takes soup as argument."""
 
-	td_header = ["t1sa2 t1sb3", "t2sa2 t2sb3"]
+	td_headers = ["t1sa2 t1sb3", "t2sa2 t2sb3"]
 
-	party_votes = soup.find_all("td", {"class":"cislo", "headers":td_header})
+	party_votes = soup.find_all("td", {"class":"cislo", "headers":td_headers})
 
 	party_votes_list = [votes.contents[0] for votes in party_votes]	
 
 	return party_votes_list
 
 
-def process_links(base_link, generated_link, codes, names):
+def process_links(generated_link, codes, names):
 	"""Scrapes and parses generated links.
-	Takes the base link and base_link_ops function returns as arguments."""
+	Takes the base_link_ops function returns as arguments."""
 
 	# In this list we assemble the final table of data and header.
 	final_table = []
@@ -191,8 +191,7 @@ def main():
 
 	ops_return = base_link_ops(base_link)
 
-	proc_links = process_links(base_link, 
-							   ops_return[0], 
+	proc_links = process_links(ops_return[0], 
 							   ops_return[1], 
 							   ops_return[2]
 							   )
